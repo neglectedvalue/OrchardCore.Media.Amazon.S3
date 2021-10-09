b0b00fcc5c714ca73f052bcae6bc8c9558d47d54
ngv <funkycatz@yandex.ru>
2021-10-10 01:56:15 +0300

    AWS lib version update. Responses validation helper

---
+++ src/OrchardCore.FileStorage.Amazon.S3/AwsResponseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.Runtime;
+
+namespace OrchardCore.FileStorage.Amazon.S3
+{
+    using System.Net;
+    
+    public static class AwsResponseExtensions
+    {
+        public static bool IsSuccessful(this AmazonWebServiceResponse response)
+        {
+            return response.HttpStatusCode == HttpStatusCode.OK;
+        }
+    }
+}
